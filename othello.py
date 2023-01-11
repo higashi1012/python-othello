80eb194e274694258750a4101e56ebb84fdae8ab
higashi1012 <hi9ga8shi7@gmail.com>
2023-01-11 14:49:27 +0900

    Update otello.py

--- othello.py
+++ othello.py
@@ -14,11 +14,11 @@ def turn():
     player, opponent = opponent, player
 
 def count(piece: str, board: list):
-    if not isinstance(piece, str):
+    if not (isinstance(piece, str) and isinstance(board, list)):
         raise TypeError()
     return sum(cell == piece for row in board for cell in row)
 
-def show_board(board, WHITE, BLACK):
+def show_board(WHITE, BLACK, board):
     print('YX1 2 3 4 5 6 7 8')
     for y, row in enumerate(board, 1):
         print(y, *row)
@@ -26,7 +26,9 @@ def show_board(board, WHITE, BLACK):
     black = count(BLACK, board)
     print(f'{BLACK}:{black} vs {WHITE}:{white}'.rjust(17))
 
-def reversible_places(x, y, dx, dy, piece, SPACE):
+def reversible_places(x, y, dx, dy, piece: str, SPACE: str):
+    if not (isinstance(piece, str) and isinstance(SPACE, str)):
+        raise TypeError()
     places = []
     x, y = x + dx, y + dy
     while 0 <= x < 8 and 0 <= y < 8 and board[y][x] != SPACE:
@@ -36,16 +38,16 @@ def reversible_places(x, y, dx, dy, piece, SPACE):
         x, y = x + dx, y + dy
     return []
 
-def putable_places(piece):
+def putable_places(piece, SPACE, DIRECTION_xy, board):
     return [(x, y) for x in range(8) for y in range(8)
             if board[y][x] == SPACE
             if any(reversible_places(x, y, dx, dy, piece, SPACE)
                 for dx, dy in DIRECTION_xy)]
 
-def playable():
-    return putable_places(BLACK) or putable_places(WHITE)
+def playable(SPACE, DIRECTION_xy, board):
+    return putable_places(BLACK, SPACE, DIRECTION_xy, board) or putable_places(WHITE, SPACE, DIRECTION_xy, board)
 
-def put_and_reverse(x, y, DIRECTION_xy, board):
+def put_and_reverse(x, y, player, SPACE, DIRECTION_xy, board):
     board[y][x] = player
     for dx, dy in DIRECTION_xy:
         for rx, ry in reversible_places(x, y, dx, dy, player, SPACE):
@@ -70,23 +72,23 @@ def show_result(WHITE, BLACK, board):
     print(f'{judge}です。お疲れさまでした。')
 
 def play():
-    while playable():
-        show_board(board, WHITE, BLACK)
-        putable_places_ = putable_places(player)
+    while playable(SPACE, DIRECTION_xy, board):
+        show_board(WHITE, BLACK, board)
+        putable_places_ = putable_places(player, SPACE, DIRECTION_xy, board)
         if not putable_places_:
             print(f'{player}は置ける場所がありません。パスします。')
             turn()
-            putable_places_ = putable_places(player)
+            putable_places_ = putable_places(player, SPACE, DIRECTION_xy, board)
         print(f'{player}の番です。')
         print('置ける場所(X,Y)は', *[(x+1,y+1) for x, y in putable_places_], 'です。')
         x = choose_place('X座標を入力してください。') - 1
         y = choose_place('Y座標を入力してください。') - 1
         if (x, y) in putable_places_:
-            put_and_reverse(x, y, DIRECTION_xy, board)
+            put_and_reverse(x, y, player, SPACE, DIRECTION_xy, board)
             turn()
         else:
             print('その場所には置けません。')
-    show_board(board, WHITE, BLACK)
+    show_board(WHITE, BLACK, board)
     show_result(WHITE, BLACK, board)
 
 
