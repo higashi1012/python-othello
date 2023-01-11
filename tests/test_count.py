d5d951ec6dd61e41f2d9a3fad824311fa42c05e0
yuhei_kajima <yuhei_kajima@nuco.co.jp>
2023-01-11 10:54:36 +0900

    add test_count_invalid_params

--- tests/test_count.py
+++ tests/test_count.py
@@ -1,3 +1,4 @@
+import pytest
 from othello import count
 
 
@@ -5,3 +6,15 @@ def test_count():
     piece = 'BLACK'
     board = [['BLACK', 'BLACK', 'WHITE', 'BLACK']]
     assert count(piece, board) == 3
+    assert count(
+        'hoge',
+        [['hoge']]
+    ) == 1
+
+
+def test_count_invalid_params():
+    with pytest.raises(TypeError):
+        count(120, 'hoge')
+
+    with pytest.raises(TypeError):
+        count('hoge', 'fuga')
